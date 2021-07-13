8c8a6db5b6cfa44ac8d43b619d6c44c167d35cbe
ArtemKhrulkov <artemhrulkov@gmail.com>
2021-07-13 18:04:19 +0500

    Sign Page Refactor (#2)
    * feat: added Sing In Page
    
    * feat: fixed a reducer's bug
    
    * feat: renamed Sign -> Authorization; small fixes;

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Sign from 'features/sign/Sign';
+import Authorization from 'features/authorization/Authorization';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -9,13 +9,11 @@ import {
   Redirect,
 } from 'react-router-dom';
 import './i18n';
-import Button from '@material-ui/core/Button';
-import i18next from 'i18next';
 
-function SignPage() {
+function AuthorizationPage() {
   return (
     <React.Fragment>
-      <Sign />
+      <Authorization />
     </React.Fragment>
   );
 }
@@ -24,25 +22,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <div className="App-header">
-          <div>
-            <Button
-              color="primary"
-              onClick={() => i18next.changeLanguage('ru')}
-            >
-              RU
-            </Button>
-            /
-            <Button
-              color="primary"
-              onClick={() => i18next.changeLanguage('en')}
-            >
-              EN
-            </Button>
-          </div>
-        </div>
         <Switch>
-          <Route exact path="/" render={SignPage} />
+          <Route exact path="/" render={AuthorizationPage} />
           <Redirect to="/" />
         </Switch>
       </div>
